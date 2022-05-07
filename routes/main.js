d8f830b2a4acc58bbc3f77459f7e5e0d2e2cc100
cwy06 <80168342+wyoung163@users.noreply.github.com>
2022-05-07 12:11:12 +0900

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -17,8 +17,8 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
   host: "127.0.0.1",
   port: "3306",
-  user: "root",
-  password: "PleaseBetter11",
+  user: "",
+  password: "",
   database: "meow",
   connectionLimit: 50
 });
