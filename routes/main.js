05d9ae16d16ecee7bc1ded88b8f2a4815699440f
Gongdongjang <jeesoo0908@gmail.com>
2022-06-05 23:29:43 +0900

    [기능추가] 인기 게시글 랭킹 및 로그인하지 않아도 게시글 볼 수 있도록 수정

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,37 @@
 const express = require("express");
+const pool = require("../db");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const title = "Meow";
+  const postRank = await pool.query(
+    `SELECT post_num, place_photo, view_count, user_id FROM post`
+  );
+  let rank5 = new Array(); //상위 5개 게시글 랭킹 보이기
+  arr = postRank[0]; //view_count만으로 정렬하기 위한 배열
+  arr.sort((a, b) => b.view_count - a.view_count); //오름차순 정렬
+  if (arr.length > 5) {
+    for (let i = 0; i < 5; i++) {
+      rank5[i] = arr[i];
+    }
+  } else {
+    rank5 = arr; //게시글이 5개 이하일 때
+  }
+  // console.log(rank5);
+  // console.log(arr[0]);
 
   if (req.session.user) {
     const nickname = req.session.user["nickname"];
     const userid = req.session.user["userid"];
     // console.log(nickname);
-    res.render("main", {title: title, nickname: nickname, userid: userid});
+    res.render("main", {
+      title: title,
+      nickname: nickname,
+      userid: userid,
+      rank5: rank5,
+    });
   } else {
-    res.render("loginMain", {title: title});
+    res.render("loginMain", {title: title, rank5: rank5, userid: "null"});
   }
 });
 
