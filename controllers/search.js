cbde0c595c5bac0565f29840e7b89f9c5f7fb171
Gongdongjang <jeesoo0908@gmail.com>
2022-06-13 00:41:35 +0900

    [코드수정] 검색기능 추가 및 css 변경

---
+++ controllers/search.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var router = express.Router();
+const session = require("express-session");
+const pool = require("../db");
+var path = require("path");
+var url = require("url");
+
+// 메뉴 기준 검색
+router.get("/search", async (req, res, next) => {
+  var title = "검색";
+  let myid;
+  if (req.session.user) {
+    myid = req.session.user["userid"];
+    nickname = req.session.user["nickname"];
+  } else {
+    myid = undefined;
+  }
+  var search_word = url.parse(req.url, true).query.search_word;
+  console.log(search_word);
+
+  try {
+    const searchD = await pool.query(
+      `SELECT post_num, place_photo, place_name, user_id FROM post as po JOIN place as pl ON po.place_num = pl.place_num WHERE menu_name LIKE ?; `,
+      ["%" + search_word + "%"]
+    );
+    // if(req.session.user)
+    var postNum = [];
+    var placePhoto = [];
+    var usersId = [];
+    var placeName = [];
+
+    for (var data of searchD[0]) {
+      postNum.push(data.post_num);
+      placePhoto.push(data.place_photo);
+      usersId.push(data.user_id);
+      placeName.push(data.place_name);
+    }
+
+    // console.log(postNum);
+
+    res.render("board/searchBoard", {
+      title: title,
+      userid: myid,
+      postNum: postNum,
+      placePhoto: placePhoto,
+      search_word: search_word,
+      usersId: usersId,
+      placeName: placeName,
+    });
+  } catch (err) {
+    console.err(err);
+  }
+});
+
+module.exports = router;
