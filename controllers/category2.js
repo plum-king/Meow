5ed36d7494ad2f3a268c36ba3c08a9e0321a8156
Gongdongjang <jeesoo0908@gmail.com>
2022-06-14 04:05:08 +0900

    [코드수정] category & hover css 수정

--- controllers/category2.js
+++ controllers/category2.js
@@ -4,27 +4,27 @@ const router = express.Router();
 const pool = require("../db.js");
 
 router.get("/category2", async (req, res, next) => {
-    const userid = req.session.user["userid"];
-    const nickname = req.session.user["nickname"];
+  const userid = req.session.user["userid"];
+  const nickname = req.session.user["nickname"];
 
-    try {
-        const data1 = await pool.query(
-            "SELECT * FROM Post WHERE user_id IN (SELECT user_id From User WHERE gender = '남')",
-        );
-        const data2 = await pool.query(
-            "SELECT * FROM Post WHERE user_id IN (SELECT user_id From User WHERE gender = '여')",
-        );
+  try {
+    const data1 = await pool.query(
+      "SELECT * FROM Post as po JOIN place as pl ON po.place_num = pl.place_num WHERE user_id IN (SELECT user_id From User WHERE gender = '남')"
+    );
+    const data2 = await pool.query(
+      "SELECT * FROM Post as po JOIN place as pl ON po.place_num = pl.place_num WHERE user_id IN (SELECT user_id From User WHERE gender = '여')"
+    );
 
-        res.render("category/category2", {
-            title: "카테고리",
-            nickname: nickname,
-            userid: userid,
-            data1: data1[0],
-            data2: data2[0],
-        });
-    } catch (err) {
-        console.error(err);
-    }
+    res.render("category/category2", {
+      title: "카테고리",
+      nickname: nickname,
+      userid: userid,
+      data1: data1[0],
+      data2: data2[0],
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
